0b1c3db6ccd41cc5d70ea8f3e4a98ce5c3e3a0c9
James Strassburg <jstrassburg@gmail.com>
2013-06-21 12:03:03 -0500

    Initial commit after creating the project from map/reduce IDEA template.

---
+++ test/com/directsupply/MisspelledSearchTermWordCount/MapTest.java
@@ -0,0 +1,11 @@
+package com.directsupply.MisspelledSearchTermWordCount;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapTest {
+    @Test
+    public void testMap() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
