ddf83a78fd302fb255d02b2c23f78a600f115675
James Strassburg <jstrassburg@gmail.com>
2013-08-07 22:30:45 -0500

    Adding support for putting job codes in the output.

--- src/com/directsupply/MisspelledSearchTermWordCount/Map.java
+++ src/com/directsupply/MisspelledSearchTermWordCount/Map.java
@@ -10,11 +10,17 @@ public class Map extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         Text outputValue = new Text("1");
+        String[] fields = value.toString().split("\t");
+        if (fields.length < 2)
+            return;
+        String jobCode = fields[1];
 
         String query = QueryMatcher.extractSearchQuery(value.toString());
         if (query != null) {
-            Text outputKey = new Text(query);
+            Text outputKey = new Text(String.format("All|%s", query));
+            Text outputKey2 = new Text(String.format("%s|%s", jobCode, query));
             context.write(outputKey, outputValue);
+            context.write(outputKey2, outputValue);
         }
     }
 }
--- test/com/directsupply/MisspelledSearchTermWordCount/MapTest.java
+++ test/com/directsupply/MisspelledSearchTermWordCount/MapTest.java
@@ -11,17 +11,22 @@ import static org.mockito.Mockito.verify;
 public class MapTest extends TestCase {
     public void testMap() throws Exception {
         final String query = "theQuery";
+        final String jobCode = "AD";
+
         LongWritable key = new LongWritable();
-        Text value = new Text(String.format("&q=(%s)&", query));
+        Text value = new Text(String.format("FOO\t%s\t&q=(%s)&", jobCode, query));
 
         Mapper.Context context = mock(Mapper.Context.class);
 
-        Text expectedKey = new Text(query.toLowerCase());
+        Text expectedKey = new Text(String.format("%s|%s", "All", query.toLowerCase()));
         Text expectedValue = new Text("1");
+
+        Text expectedKey2 = new Text(String.format("%s|%s", jobCode, query.toLowerCase()));
 
         Map map = new Map();
         map.map(key, value, context);
 
         verify(context).write(expectedKey, expectedValue);
+        verify(context).write(expectedKey2, expectedValue);
     }
 }
